0891408d2915107bcf7d9977c0c083a67a89e3d2
Jingxuan Kang <jxkang01@gmail.com>
2022-06-30 16:14:26 +0100

    reve test_fid

--- scripts/test_fid.sh
+++ scripts/test_fid.sh
@@ -1,8 +1,8 @@
 set -ex
 python test_fid.py \
---dataroot /home/jingxuankang/dl/F-LSeSim/datasets/horse2zebra \
---checkpoints_dir ./checkpoints_forcross_similarity \
---name horse2zebra \
+--dataroot /home/jingxuankang/dl/F-LSeSim/datasets/cityscapes \
+--checkpoints_dir ./city_check  \
+--name cityscapes \
 --gpu_ids 0 \
 --model sc \
 --num_test 0
